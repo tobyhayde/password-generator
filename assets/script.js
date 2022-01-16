34b6006a4eba6b26ce691036006c9faed596b822
thayde <toby.hayde@icloud.com>
2022-01-16 11:35:18 -0600

    password length prompt added

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,32 @@
 // Assignment code here
 
+// 
+let generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+// 
+    let smallLetters = "abcdefghijklmnopqrstuvwxyz";
+    let capLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numbers = "0123456789";
+    let specialChar = "~!@#]$%=^[&*()_+{}:?><;.,";
+    let password = "";
+    let availChar = "";
+
+let passwordLength = prompt("How many characters would you like your password to be? Please choose a password length between 8 and 128.")
+
+if (passwordLength >= 8 && passwordLength <= 128) {
+    console.log(passwordLength)
+}
+
+
+
+else {
+    alert("Your password must be between 8 and 128 characters long")
+    return "";
+}
+}
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
